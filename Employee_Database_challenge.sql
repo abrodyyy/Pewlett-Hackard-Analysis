b3a4979e8f4e0dbd76af5d16e882dc8b047c7d61
abrodyyy <alexandra.brody@gmail.com>
2023-01-10 00:00:04 -0800

    updates

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,90 +1,21 @@
--- DELIVERABLE 1: 
--- Create a table that retreives the number of retiring employees by title.
+-- Deliverable 1: The Number of Retiring Employees by Title (50 points)
+-- A query is written and executed to create a Retirement Titles table for 
+-- employees who are born between January 1, 1952 and December 31, 1955:
 
 SELECT e.emp_no,
-	   e.first_name,
-	   e.last_name,
-	   t.title,
-	   t.from_date,
-	   t.to_date
-INTO retirement_titles
-FROM employees as e
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-order by e.emp_no;
+e.first_name,
+e.last_name,
+t.title
+t.from_date
+t.to_date
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
-INTO unique_titles
-FROM retirement_titles
-WHERE (to_date = '9999-01-01') 
-ORDER BY emp_no ASC, to_date DESC;
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
 
--- Retreive the number of employees by their most recent job title who are about to retire
-SELECT COUNT(ut.emp_no),
-ut.title
-INTO retiring_titles
-FROM unique_titles as ut
-GROUP BY title
-ORDER BY COUNT(title) DESC;
-
--- DELIVERABLE 2
--- Create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program
-SELECT DISTINCT ON(e.emp_no) e.emp_no,
-	e.first_name,
-	e.last_name,
-	e.birth_date,
-	de.from_date,
-	de.to_date,
-	t.title
---INTO mentorship_eligibility
-FROM employees as e
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-	AND (de.to_date = '9999-01-01')
-ORDER BY e.emp_no;
-
--- DELIVERABLE 3
--- create 2 additional queries to further the analysis
-
--- group mentors by title
-SELECT COUNT(me.emp_no),
-me.title
-INTO mentorship_eligibility_by_title
-FROM mentorship_eligibility as me
-GROUP BY title
-ORDER BY COUNT(title) DESC;
-
--- group mentors by title and expand elibibilty date from 63-65
-SELECT DISTINCT ON(e.emp_no) e.emp_no,
-	e.first_name,
-	e.last_name,
-	e.birth_date,
-	de.from_date,
-	de.to_date,
-	t.title
-INTO mentorship_eligibility2
-FROM employees as e
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1964-01-01' AND '1965-12-31')
-	AND (de.to_date = '9999-01-01')
-ORDER BY e.emp_no;
-
-SELECT COUNT(me.emp_no),
-me.title
---INTO mentorship_eligibility_by_title2
-FROM mentorship_eligibility as me
-GROUP BY title
-ORDER BY COUNT(title) DESC;
-
-SELECT * FROM mentorship_eligibility;
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
