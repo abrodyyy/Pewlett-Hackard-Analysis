0138f279862daf74d28f937e9b3827ad1d823ef2
abrodyyy <alexandra.brody@gmail.com>
2023-01-10 01:16:45 -0800

    updating queries

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -22,7 +22,7 @@ SELECT DISTINCT ON (emp_no) emp_no,
     last_name,
     title INTO unique_titles
 FROM retirement_titles
-WHERE (to_date = '9999-01-01')
+WHERE to_date = '9999-01-01'
 ORDER BY emp_no,
     to_date DESC;
 
@@ -33,3 +33,25 @@ SELECT COUNT(ut.emp_no),
 FROM unique_titles as ut
 GROUP BY Title
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program (30 points)
+
+-- A query is written and executed to create a Mentorship Eligibility table for 
+-- current employees who were born between January 1, 1965 and December 31, 1965:
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title INTO mentorship_eligibilty
+FROM employees as e
+    LEFT JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+    LEFT JOIN titles as t ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+    AND (
+        e.birth_date BETWEEN '1965-01-01'
+        AND '1965-12-31'
+    )
+ORDER BY emp_no;
+-- The Mentorship Eligibility table is exported and saved as mentorship_eligibilty.csv. (5 pt)
