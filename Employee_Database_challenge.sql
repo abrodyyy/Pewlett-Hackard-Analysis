7ad47ebe27d40a1a6891e41afb8257db652fdda1
abrodyyy <alexandra.brody@gmail.com>
2023-01-10 23:52:48 -0800

    final updates

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -55,3 +55,33 @@ WHERE (de.to_date = '9999-01-01')
     )
 ORDER BY emp_no;
 
+-- Count of employees eligible for mentorship program by title
+SELECT COUNT(ut.emp_no),
+    ut.title INTO mentorship_eligibilty_titles
+FROM mentorship_eligibilty as ut
+GROUP BY Title
+ORDER BY COUNT(title) DESC
+
+-- New Mentorship Eligibility table for current employees 
+-- who were born between January 1, 1960 and December 31, 1965:
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title INTO mentorship_eligibilty_new
+FROM employees as e
+    INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+    INNER JOIN titles as ti ON (e.emp_no = ti.emp_no)
+WHERE (
+        e.birth_date BETWEEN '1960-01-01' AND '1965-12-31'
+    )
+    AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no DESC
+-- Count of employees eligible for mentorship program by title - new criteria
+SELECT COUNT(ut.emp_no),
+    ut.title INTO mentorship_eligibilty_titles_new
+FROM mentorship_eligibilty_new as ut
+GROUP BY Title
+ORDER BY COUNT(title) DESC
